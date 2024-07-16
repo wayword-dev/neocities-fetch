615b6673a9fbd083ebfe2a51f6c3872284e3f25a
wayword <soul@wayword.dev>
2024-07-16 09:24:38 -0500

    target messages

--- fetch.js
+++ fetch.js
@@ -10,7 +10,7 @@
     console.log("PARENT: sending message")
     frame.contentWindow.postMessage("MESSAGE FROM PARENT")
     window.addEventListener("message", (e) => {
-      if (window.srcElement !== frame) return
+      if (window.srcElement !== frame) return console.log("PARENT: ignroing")
       console.log("PARENT: got message from child", e)
     })
   })
