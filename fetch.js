ecc5b6c680fda02f54e8424fcd369f624c37e700
wayword <soul@wayword.dev>
2024-07-16 09:05:10 -0500

    add parent/child

--- fetch.js
+++ fetch.js
@@ -1,11 +1,16 @@
-(function () {
+/* (function () { */
   const frame = document.createElement("iframe")
   frame.style.position = "absolute"
   frame.style.width = "0"
   frame.style.height = "0"
   frame.style.border = "0"
+  frame.src = "/neocities-fetch/frame.html"
   console.log("hi")
   addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(frame)
+    frame.contentWindow.postMessage("MESSAGE FROM PARENT")
+    frame.contentWindow.addEventListener("message", (e) => {
+      console.log("PARENT: got message from child", e)
+    })
   })
-})()
+/* })() */
