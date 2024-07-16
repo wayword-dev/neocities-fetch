182381d702b0405faa7a10893efdb860782d226a
wayword <soul@wayword.dev>
2024-07-16 11:17:25 -0500

    hmmm readystate weird

--- fetch.js
+++ fetch.js
@@ -1,31 +1,30 @@
 const nfetch = (function () {
-  const frame = document.createElement("iframe")
   let ready = false
+  window.addEventListener("message", (e) => {
+    if (event.source !== frame.contentWindow) return
+    if (event.data.ready) {
+      ready = true
+      for (const req of preDomReqs) {
+        frame.contentWindow.postMessage(req)
+      }
+    } else {
+      pendingRequests[e.data.requestId](e.data.data)
+      delete pendingRequests[e.data.requestId]
+    }
+  })
+
+  const frame = document.createElement("iframe")
   frame.style.position = "absolute"
   frame.style.width = "0"
   frame.style.height = "0"
   frame.style.border = "0"
   frame.src = "https://wayword-dev.github.io/neocities-fetch/frame.html"
-  function setup() {
-    document.body.appendChild(frame)
-    window.addEventListener("message", (e) => {
-      if (event.source !== frame.contentWindow) return
-      if (event.data.ready) {
-        ready = true
-        for (const req of preDomReqs) {
-          frame.contentWindow.postMessage(req)
-        }
-      } else {
-        pendingRequests[e.data.requestId](e.data.data)
-        delete pendingRequests[e.data.requestId]
-      }
-    })
-  }
+
   if (document.readyState === "interactive" || document.readyState === "complete") {
-    setup()
+    document.body.appendChild(frame)
   } else {
     addEventListener("DOMContentLoaded", () => {
-      setup()
+      document.body.appendChild(frame)
     })
   }
 
